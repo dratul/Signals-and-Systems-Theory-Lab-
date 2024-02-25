c6c2738e3053f68fd63c4f6d61924317ed6ed83f
dratul <atul.join@gmail.com>
2024-02-25 07:03:33 +0530

    updated at Home

---
+++ my_git.py
@@ -0,0 +1,4 @@
+import git 
+repo = git.Repo("D:\OneDrive - bietjhs.ac.in\TEACHING\B_Tech\Signals_and_systems_theory_lab\Github_repo_shared_theory_lab\Signals-and-Systems-Theory-Lab-") 
+ss = repo.remote(name='Signals-and-Systems-Theory-Lab-') 
+origin.pull()
